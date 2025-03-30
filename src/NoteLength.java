a4e678ac664b022559e37c3e87deacec8c82ee96
Jack Holy <jholy@carroll.edu>
2025-03-30 15:53:41 -0600

    Move enums to their own file

---
+++ src/NoteLength.java
@@ -0,0 +1,18 @@
+package src;
+
+public enum NoteLength {
+    WHOLE(1.0f),
+    HALF(0.5f),
+    QUARTER(0.25f),
+    EIGHTH(0.125f);
+
+    private final int timeMs;
+
+    private NoteLength(float length) {
+        timeMs = (int)(length * Note.MEASURE_LENGTH_SEC * 1000);
+    }
+
+    public int timeMs() {
+        return timeMs;
+    }
+}
